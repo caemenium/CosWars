37173cb2fe2976e840b251c5e60842ad2a1f4672
Abdullah Karakapıcı <akarakapici@hotmail.com>
2017-02-06 16:09:28 +0300

    yeni about2

--- CosWars/Controllers/HomeController.cs
+++ CosWars/Controllers/HomeController.cs
@@ -79,6 +79,12 @@ namespace CosWars.Controllers
 
             return View();
         }
+        public ActionResult About2()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
